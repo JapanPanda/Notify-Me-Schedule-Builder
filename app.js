466d64a388a135a1b9f8a3b7b11ba83651d2950f
JapanPanda <japanxpanda@yahoo.com>
2018-11-24 14:27:38 -0800

    Implemented class data grabbing and writing the spots open in results.json

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ var rp = rp.defaults({
   followAllRedirects: true
 });
 
+var $ = require('cheerio');
 const puppeteer = require('puppeteer');
-const $ = require('cheerio');
 const fs = require('fs');
+const chalk = require('chalk');
 const tokens = require('./tokens.json');
 const classes = require('./classes.json');
 const pushbulleturl = 'https://api.pushbullet.com/v2/pushes';
@@ -39,7 +40,6 @@ function pushBulletTest() {
       console.log('PushBullet Post Failed!');
       console.log(err);
     });
-  exit();
 }
 
 function getTerm(date) {
@@ -47,36 +47,37 @@ function getTerm(date) {
     throw new Error('Date is null!');
   }
   if (date.getMonth() >= 9 && date.getMonth() <= 11) {
-    console.log('Entering into Winter quarter since it is month ' + date.getMonth() + '...');
+    console.log(chalk.cyan('Entering into Winter quarter since it is month ' + date.getMonth() + '...'));
     return (parseInt(date.getFullYear()) + 1) + '01';
   }
   else if (date.getMonth() >= 1 && date.getMonth() <= 3) {
-    console.log('Entering into Spring quarter since it is month ' + date.getMonth() + '...');
+    console.log(chalk.cyan('Entering into Spring quarter since it is month ' + date.getMonth() + '...'));
     return (parseInt(date.getFullYear())) + '03';
   }
   else if (date.getMonth() >= 4 && date.getMonth() <= 6) {  // NOt sure how summer registration works, will confirm later
-    console.log('Entering into Summer Session I since it is month ' + date.getMonth() + '...');
+    console.log(chalk.cyan('Entering into Summer Session I since it is month ' + date.getMonth() + '...'));
     return 'Summer Session I ' + (parseInt(date.getFullYear()));
   }
   else if (date.getMonth() >= 7 && date.getMonth() <= 9) {
-    console.log('Entering into Fall Quarter since it is month ' + date.getMonth() + '...');
+    console.log(chalk.cyan('Entering into Fall Quarter since it is month ' + date.getMonth() + '...'));
     return (parseInt(date.getFullYear()) + 1) + '10';
   }
   else {
-    console.log('It\'s a bit early to set up this program right now for Spring quarter registration...\nTry again next month');
+    console.log(chalk.red('It\'s a bit early to set up this program right now for Spring quarter registration...\nTry again next month'));
     throw new Error('Too early!');
   }
 }
 
 // Had to use a headless browser since request wasn't good enough to load the client-side javascript
-async function schedulebuilderLogin() {
-  console.log('Attempting to log into Schedule Builder...');
+async function sbInit() {
+  console.log(chalk.cyan('Attempting to log into Schedule Builder...'));
 
   await puppeteer.launch().then(async browser => {
     try {
       var username = tokens.username;
       var password = tokens.password;
       const page = await browser.newPage();
+      // Login
       await page.goto(sburl);
       await page.evaluate((tokens) => {
         document.querySelector('#username').value = tokens.username;
@@ -84,48 +85,82 @@ async function schedulebuilderLogin() {
         document.querySelector('#submit').click();
       }, tokens);
       await page.waitForNavigation();
-      console.log('Logged into Schedule Builder successfully!');
+      // Enter a quarter
       var date = new Date;
       var dateString = getTerm(date);
-      console.log(dateString);
-      // TO DO: redirect to the main picking schedule page
       await Promise.all([
         page.select('select#termCode1', dateString),
         page.waitForNavigation(),
         page.click('button')
       ]);
-      await page.screenshot({path: './debug/screenshots/screenshot.png'});
+
+      console.log(chalk.cyan('Successfully entered the quarter\n'));
+      var resultsJSON = {};
+
+      // For the non-specific section classes
+      for (const currClass of classes.classes) {
+        await page.$eval('#inline_course_number', (ele, currClass) => ele.value = currClass, currClass);
+        await page.click('button[onclick="javascript:UCD.SAOT.COURSES_SEARCH_INLINE.textSearch();"]');
+        await page.waitFor(1000);
+        var divs = await page.$$('.data-item-short');
+        if (divs.length == 0) {
+            // Theres a weird bug here where the results don't show up sometime
+            console.log(chalk.red('Could not load the search results, trying again...'));
+            await page.screenshot({path: './debug/screenshots/error.png'});
+            await fs.writeFileSync('error.json', await page.evaluate(() => document.body.innerHTML), 'utf8');
+            await Promise.all([
+              page.waitFor(3000),
+              page.click('button[onclick="javascript:UCD.SAOT.COURSES_SEARCH_INLINE.textSearch();"]')
+            ]);
+        }
+
+        var classesJSON = [];
+        for (const div of divs) {  // Separate the class divs and break it down into JSON objects
+          var currObj = await (await div.getProperty('innerHTML')).jsonValue();
+          $ = $.load(currObj);
+          var className = $('.data-row').eq(0).text().split(':')[1].split('-')[1].substring(1);
+          className = className.substring(0, className.length - 1);
+          var classSpots = $('.data-column').eq(1).text().split(':')[1].substring(1);
+          var parsedObj = {
+            'class_name': className,
+            'class_spots': classSpots
+          }
+          classesJSON.push(parsedObj);
+        }
+
+        console.log(chalk.cyan('Logging JSON for ' + currClass + ' into results.json...'));
+        resultsJSON[currClass] = classesJSON;
+      }
+      await fs.writeFileSync('results.json', JSON.stringify(resultsJSON, null, 2), 'utf8');
       await browser.close();
     }
     catch (err) {
-      console.log('Something went wrong inside of Puppeteer...');
-      console.log(err);
-      console.log('Closing Puppeteer...');
+      console.log(chalk.red('Something went wrong inside of Puppeteer...'));
+      console.log(chalk.red(err));
+      console.log(chalk.red('Closing Puppeteer...'));
       await browser.close();
     }
   });
   // Posting to the search url results in some really really hard to parse (unreadable) information, so we'll just use puppeteer to scrape
-  // see if theres a way to be cool and use request (i think this is a pain and should not be so)
 }
 
 async function start()
 {
-  console.log('Starting the server now...');
-  console.log('Attempting to read tokens.json file...');
-  console.log('Username: ' + tokens.username);
-  console.log('Password: ' + tokens.password);
-  console.log('PushBullet Token: ' + tokens.pushbullet_token + '\n');
-  console.log('Attempting to read classes.json file...');
-  console.log('Classes (non-specific section): ' + classes.classes);
-  console.log('Specific Sections: ' + classes.specific_sections + '\n');
-  console.log('Logging into Schedule Builder...');
-  await schedulebuilderLogin();  // Good ol' procedural programming
+  console.log(chalk.cyan('Starting the server now...'));
+  console.log(chalk.cyan('Attempting to read tokens.json file...'));
+  console.log(chalk.blueBright('Username: ') + tokens.username);
+  console.log(chalk.blueBright('Password: ') + tokens.password);
+  console.log(chalk.blueBright('PushBullet Token: ') + tokens.pushbullet_token + '\n');
+  console.log(chalk.cyan('Attempting to read classes.json file...'));
+  console.log(chalk.blueBright('Classes (non-specific section): ') + classes.classes);
+  console.log(chalk.blueBright('Specific Sections: ') + classes.specific_sections + '\n');
+  await sbInit();  // Good ol' procedural programming
   exit();
 }
 
 function exit()
 {
-  console.log('Closing server now...');
+  console.log(chalk.red('Closing server now...'));
 }
 
 start();
